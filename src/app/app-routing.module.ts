844a180da28e67a216eb0079e8f55146bf7582f7
Elena Diaz Garrido <elena.diazgarrido@gmail.com>
2021-03-04 21:05:19 +0100

    Componentes main, event creados. Componentes hijos home, header, footer, etc creados. Routing principal creado.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventComponent } from './components/event/event.component';
+import { DetailComponent } from './components/main/detail/detail.component';
+import { NewComponent } from './components/main/new/new.component';
+import { HomeComponent } from './components/main/home/home.component';
+import { LoginComponent } from './components/main/login/login.component';
+import { MainComponent } from './components/main/main.component';
+import { RegisterComponent } from './components/main/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainComponent, children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: 'new-event', component: NewComponent },
+      { path: 'event/:event_id', component: DetailComponent },
+    ]
+  },
+  {
+    path: 'event/:event-name', component: EventComponent, children: [
+      { path: 'home', },
+      { path: 'description', },
+      { path: 'contact', },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { HomeComponent } from './components/main/home/home.component';
+import { LoginComponent } from './components/main/login/login.component';
+import { RegisterComponent } from './components/main/register/register.component';
+import { NewComponent } from './components/main/new/new.component';
+import { DetailComponent } from './components/main/detail/detail.component';
+import { EventDescriptionComponent } from './components/event/description/event-description.component';
+import { EventHomeComponent } from './components/event/home/event-home.component';
+import { ContactoComponent } from './components/event/contacto/contacto.component';
+import { HeaderComponent } from './components/main/header/header.component';
+import { HeroComponent } from './components/main/hero/hero.component';
+import { FooterComponent } from './components/main/footer/footer.component';
+import { CardEventComponent } from './components/main/card-event/card-event.component';
+import { ListEventsComponent } from './components/main/list-events/list-events.component';
+import { EventHeaderComponent } from './components/event/event-header/event-header.component';
+import { EventFooterComponent } from './components/event/event-footer/event-footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    NewComponent,
+    DetailComponent,
+    EventDescriptionComponent,
+    EventHomeComponent,
+    ContactoComponent,
+    HeaderComponent,
+    HeroComponent,
+    FooterComponent,
+    CardEventComponent,
+    ListEventsComponent,
+    EventHeaderComponent,
+    EventFooterComponent,
   ],
   imports: [
     BrowserModule,
