9829ed55930f53712026ee8a0eee9685d6b2cf8a
Elena Diaz Garrido <elena.diazgarrido@gmail.com>
2021-03-06 23:17:11 +0100

    Header con scroll en css y js acabado. Hero empezado

--- src/app/components/main/header/header.component.ts
+++ src/app/components/main/header/header.component.ts
@@ -1,4 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jQuery';
+
+
+$(window).on('scroll', function () {
+  if ($(window).scrollTop() > 20) {
+    $('nav').removeClass('bg-light');
+    $('nav').addClass('bg-dark');
+    $('nav').addClass('txt-dark');
+    $('.navbar .navbar-brand img').attr('src', 'assets/img/logo-400x55-white.png');
+  } else {
+    $('nav').removeClass('bg-dark');
+    $('nav').addClass('bg-light');
+    $('nav').removeClass('txt-dark');
+  }
+
+  if ($(window).scrollTop() < 20) {
+    $('.navbar .navbar-brand img').attr('src', 'assets/img/logo-400x55.png');
+  }
+})
 
 @Component({
   selector: 'app-header',
